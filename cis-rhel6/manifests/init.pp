21159d8a6e5669b3ffa4618fa3c26942d359d84d
Arild Jensen <ajensen@counter-attack.com>
2013-03-08 19:27:07 -0800

    Added sections 8 and 9

--- cis-rhel6/manifests/init.pp
+++ cis-rhel6/manifests/init.pp
@@ -487,5 +487,60 @@ class cis-rhel6 {
 
 # CIS Control 6.5 TODO Get pam.d/su template
 
+# CIS Control 8.1
+# CIS Control 8.2
+  file {'/etc/issue.net':
+    source  => "puppet:///modules/cis-puppet/cis-rhel6/etc/issue.net",
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+  }
+  file {'/etc/issue':
+    source  => "puppet:///modules/cis-puppet/cis-rhel6/etc/issue.net",
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+  }
+  file {'/etc/motd':
+    source  => "puppet:///modules/cis-puppet/cis-rhel6/etc/issue.net",
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+  } 
+
+# CIS 9.1.2
+# CIS 9.1.6
+  file {'/etc/passwd':
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+  }
+
+# CIS 9.1.3
+# CIS 9.1.7
+  file {'/etc/shadow':
+    owner   => root,
+    group   => root,
+    mode    => 0000,
+  }
+
+# CIS 9.1.4
+# CIS 9.1.8
+  file {'/etc/gshadow':
+    owner   => root,
+    group   => root,
+    mode    => 0000,
+  }
+
+# CIS 9.1.5
+# CIS 9.1.9
+  file {'/etc/group':
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+  }
+
+# CIS 9.1.11 NOT IMPLEMENTED 
+# CIS 9.1.12 NOT IMPLEMENTED
 
 } # End class definition
