5d65a8b5a433ff469ccaa254b773f5a8cb2973d8
Arild Jensen <ajensen@counter-attack.com>
2013-03-17 21:33:51 -0700

    Adding systemaccess to generic linux class cis::linuxcontrols

--- linuxcontrols/manifests/init.pp
+++ linuxcontrols/manifests/init.pp
@@ -499,4 +499,200 @@ class cis::linuxcontrols::c0050 {
 
 # CIS Control 5.2.12 -- Not doable with Puppet
 
+class cis::linuxcontrols::c0051 {
+
+# CIS Control 6.1.1
+  package {'cronie-anacron':
+    ensure => installed,
+  }
 }
+
+class cis::linuxcontrols::c0052 {
+
+# CIS Control 6.1.2
+  service {'cron':
+    ensure      => running,
+    enable      => true,
+    hasrestart  => true,
+    hasstatus   => true, 
+    require     => Package['cronie-anacron'],
+  }
+}
+
+class cis::linuxcontrols::c0053 {
+
+# CIS Control 6.1.3
+  file {'/etc/anacrontab':
+    owner => root,
+    group => root,
+    mode  => 0600,
+  }
+}
+
+class cis::linuxcontrols::c0054 {
+
+# CIS Control 6.1.4
+  file {'/etc/crontab':
+    owner => root,
+    group => root,
+    mode  => 0600,
+  }
+}
+
+class cis::linuxcontrols::c0055 {
+
+# CIS Control 6.1.5
+  file {'/etc/cron.hourly':
+    owner => root,
+    group => root,
+    mode  => 0600,
+  }
+}
+
+class cis::linuxcontrols::c0056 {
+
+# CIS Control 6.1.6
+  file {'/etc/cron.daily':
+    owner => root,
+    group => root,
+    mode  => 0600,
+  }
+}
+
+class cis::linuxcontrols::c0057 {
+
+# CIS Control 6.1.7
+  file {'/etc/cron.weekly':
+    owner => root,
+    group => root,
+    mode  => 0600,
+  }
+}
+
+class cis::linuxcontrols::c0058 {
+
+# CIS Control 6.1.8
+  file {'/etc/cron.monthly':
+    owner => root,
+    group => root,
+    mode  => 0600,
+  }
+}
+
+class cis::linuxcontrols::c0059 {
+
+# CIS Control 6.1.9
+  file {'/etc/cron.d':
+    owner => root,
+    group => root,
+    mode  => 0700,
+  }
+}
+
+class cis::linuxcontrols::c0060 {
+
+# CIS Control 6.1.10
+# CIS Control 6.1.11
+  file {'/etc/at.deny':
+    ensure  => absent,
+  }
+}
+
+class cis::linuxcontrols::c0061 {
+  file {'/etc/at.allow':
+    source  => "puppet:///modules/cis-puppet/cis-rhel6/etc/at.allow",
+    owner   => root,
+    group   => root,
+    mode    => 0600,
+  }
+}
+
+class cis::linuxcontrols::c0062 {
+  file {'/etc/cron.allow':
+    source  => "puppet:///modules/cis-puppet/cis-rhel6/etc/cron.allow",
+    owner   => root,
+    group   => root,
+    mode    => 0600,
+  }
+}
+
+class cis::linuxcontrols::c0063 {
+
+# CIS Control 6.2.1
+# CIS Control 6.2.2
+# CIS Control 6.2.3
+# CIS Control 6.2.4
+# CIS Control 6.2.5
+# CIS Control 6.2.6
+# CIS Control 6.2.7
+# CIS Control 6.2.8
+# CIS Control 6.2.9
+# CIS Control 6.2.10
+# CIS Control 6.2.11
+# CIS Control 6.2.12
+# CIS Control 6.2.13, please review sshd_config file as you may make changes
+# while staying in compliance
+# CIS Control 6.2.14
+
+  package { 'openssh-server':
+    ensure  => present,
+    before  => File['/etc/ssh/sshd_config'],
+  }
+}
+
+class cis::linuxcontrols::c0064 {
+
+  file { '/etc/ssh/sshd_config':
+    ensure  => present,
+    source  => "puppet:///modules/cis-puppet/el6/etc/ssh/sshd_config",
+    owner   => root,
+    group   => root,
+    mode    => 0600,
+  }
+}
+
+class cis::linuxcontrols::c0065 {
+
+  service { 'sshd':
+    ensure      => running,
+    enabled     => true,
+    hasrestart  => true,
+    hasstatus   => true,
+    subscribe   =>
+    File['/etc/ssh/sshd_config'},
+  }
+}
+
+class cis::linuxcontrols::c0066 {
+
+# CIS Control 6.3.1
+  file {'/etc/sysconfig/authconfig':
+    source  => "puppet:///modules/cis-puppet/cis-rhel6/etc/sysconfig/authconfig",
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+  }
+}
+
+class cis::linuxcontrols::c0067 {
+
+# CIS Control 6.3.2
+# CIS Control 6.3.3
+# CIS Control 6.3.6
+  file {'/etc/pam.d/system-auth':
+    ensure  => link,
+    target  => '/etc/pam.d/system-auth-ac',
+  }
+  file {'/etc/pam.d/system/auth-ac':
+    source  => "puppet:///modules/cis-puppet/cis-rhel6/etc/pam.d/system-auth-ac",
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+  }
+}
+
+class cis::linuxcontrols::c0068 {
+
+# CIS Control 6.5 TODO Get pam.d/su template
+}
+
