f488ce2aff124b9cad8e3f3a7a6c3aaec42397d7
Arild Jensen <ajensen@counter-attack.com>
2013-04-13 16:10:08 -0700

    Corrected location for grub.conf in f0000.sh

--- files/linuxcontrols/scripts/f0000.sh
+++ files/linuxcontrols/scripts/f0000.sh
@@ -3,7 +3,7 @@
 # This script is used by the cis Puppet module.
 # For the latest version see https://github.com/arildjensen/cis-puppet/
 
-COMMAND=`grep ^password /etc/grub.conf 2>/dev/null`
+COMMAND=`grep ^password /boot/grub/grub.conf 2>/dev/null`
 
 if [ x$COMMAND = x ]; 
   then 
