2d207a3abc54112cc0cac2798c68115282110262
David Arena <darena@laterpay.net>
2013-05-14 01:14:16 +0000

    Used the sysctl module

--- manifests/linuxcontrols/c0015.pp
+++ manifests/linuxcontrols/c0015.pp
@@ -19,10 +19,20 @@
 #
 
 class cis::linuxcontrols::c0015 {
-  file {'/etc/sysctl.conf':
-    source  => 'puppet:///modules/cis/el6/etc/sysctl.conf',
-    owner   => root,
-    group   => root,
-    mode    => '0640',
-  }
+
+    sysctl::value { "fs.suid_dumpable": value => "0" }
+    sysctl::value { "kernel.exec-shield": value => "1" }
+    sysctl::value { "kernel.randomize_va_space": value => "2" }
+    sysctl::value { "net.ipv4.conf.all.send_redirects": value => "0" }
+    sysctl::value { "net.ipv4.conf.default.send_redirects": value => "0" }
+    sysctl::value { "net.ipv4.conf.all.accept_source_route": value => "0" }
+    sysctl::value { "net.ipv4.conf.all.accept_redirects": value => "0" }
+    sysctl::value { "net.ipv4.conf.all.secure_redirects": value => "0" }
+    sysctl::value { "net.ipv4.conf.all.log_martians": value => "1"}
+    sysctl::value { "net.ipv4.conf.default.accept_redirects": value => "0" }
+    sysctl::value { "net.ipv4.conf.default.secure_redirects": value => "0" }
+    sysctl::value { "net.ipv4.icmp_echo_ignore_broadcasts": value => "1" }
+    sysctl::value { "net.ipv4.icmp_ignore_bogus_error_messages": value => "1" }
+    sysctl::value { "net.ipv4.conf.all.rp_filter": value => "1" }
+    sysctl::value { "net.ipv4.tcp_max_syn_backlog": value => "4096" }
 }
