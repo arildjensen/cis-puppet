da12c8c40f561cb475098ca43b4d43a682941ca4
Arild Jensen <ajensen@counter-attack.com>
2013-09-29 14:57:37 -0700

    Moved f0000 and f0001 to ruby code

--- lib/facter/f0000.rb
+++ lib/facter/f0000.rb
@@ -2,6 +2,15 @@ require 'facter'
 
 Facter.add(:f0000) do
   setcode do
-    Facter::Util::Resolution.exec("/usr/local/sbin/f0000.sh")
+    begin
+      grubconf = '/boot/grub/grub.conf'
+      if File.open(grubconf).grep(/^password/) then
+        puts "pass"
+      else
+        puts "fail"
+      end
+    rescue
+      puts "fail"
+    end
   end
 end
--- lib/facter/f0001.rb
+++ lib/facter/f0001.rb
@@ -2,6 +2,26 @@ require 'facter'
 
 Facter.add(:f0001) do
   setcode do
-    Facter::Util::Resolution.exec("/usr/local/sbin/f0001.sh")
+    begin
+      pwfile = '/etc/passwd'
+      File.open(pwfile, 'r') do |f|
+        f.each_line do |line|
+          entry_data = line.split(':')
+          if entry_data[0] != /^\+/ &&
+            entry_data[0] != "root" &&
+            entry_data[0] != "sync" &&
+            entry_data[0] != "shutdown" &&
+            entry_data[0] != "halt" &&
+            entry_data[2].to_i < 500 &&
+            entry_data[6] != "/sbin/nologin" then
+            puts "fail"
+            exit
+          end
+        end
+        puts "pass"
+      end
+    rescue
+      puts "fail"
+    end
   end
 end
