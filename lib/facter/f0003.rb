0550c8a08ff468d57a566155c25ae1b3faaa32c6
Spencer Krum <nibz@cat.pdx.edu>
2014-06-05 14:34:29 -0700

    Fixing f000[23] to look before leap
    Previously the f000[23] facts would read from their control files even
    if the control files had not yet been made.

--- lib/facter/f0003.rb
+++ lib/facter/f0003.rb
@@ -1,7 +1,9 @@
 require 'facter'
 
-Facter.add(:f0003) do
-  setcode do
-    Facter::Util::Resolution.exec("cat /var/log/control_f0003")
+if File.exist?('/var/log/control_f0003')
+  Facter.add(:f0003) do
+    setcode do
+      Facter::Util::Resolution.exec("cat /var/log/control_f0003")
+    end
   end
 end
