01c345869ba826d2b1b8351caa5e4e939618a75f
Jorrit Folmer <jorrit@nanowolk.nl>
2015-04-08 15:54:17 +0200

    CIS 1.5.3: added implementation for grub password ($cis::grubpassword). Also parameterized the cis class to make defaults easier (cis::params) and hiera() lookup calls redundant.

--- manifests/linuxcontrols/c0006.pp
+++ manifests/linuxcontrols/c0006.pp
@@ -1,22 +1,22 @@
-class cis::linuxcontrols::c0006 {
 # CIS RHEL6 Control 1.4.1
 # TODO grub2 is not included in el6
 
-  augeas { 'grub.conf/selinux': 
-    incl => '/boot/grub/grub.conf', 
-    lens => 'grub.lns', 
-    changes => [ 
-      'rm title[*]/kernel/selinux', 
-    ], 
-    onlyif => "get title[*]/kernel/selinux == '0'",
+class cis::linuxcontrols::c0006 {
+  augeas { 'grub.conf/selinux':
+    incl    => '/boot/grub/grub.conf',
+    lens    => 'grub.lns',
+    changes => [
+      'rm title[*]/kernel/selinux',
+    ],
+    onlyif  => "get title[*]/kernel/selinux == '0'",
   }
 
-  augeas { 'grub.conf/enforcing': 
-    incl => '/boot/grub/grub.conf', 
-    lens => 'grub.lns', 
-    changes => [ 
-      'rm title[*]/kernel/enforcing', 
-    ], 
-    onlyif => "get title[*]/kernel/enforcing == '0'",
+  augeas { 'grub.conf/enforcing':
+    incl    => '/boot/grub/grub.conf',
+    lens    => 'grub.lns',
+    changes => [
+      'rm title[*]/kernel/enforcing',
+    ],
+    onlyif  => "get title[*]/kernel/enforcing == '0'",
   }
 }
--- manifests/linuxcontrols/c0012.pp
+++ manifests/linuxcontrols/c0012.pp
@@ -1,3 +1,30 @@
-class cis::linuxcontrols::c0012 {
-# CIS RHEL6 Control 1.5.3 -- Not configurable via puppet
+# CIS RHEL6 Control 1.5.3 
+class cis::linuxcontrols::c0012 (
+  $grubpassword = $cis::grubpassword
+  ) {
+  if $grubpassword == undef {
+    fail('No GRUB password defined.')
+  } else {
+    # add password line to grub.conf if not exists
+    augeas { 'grub.conf/insert password':
+      incl    => '/boot/grub/grub.conf',
+      lens    => 'grub.lns',
+      changes => [
+        'ins password after default',
+        "set password/encrypted ''",
+        "set password '${grubpassword}'",
+      ],
+      onlyif  => 'match password size == 0',
+    }
+    # if password line already exists, update it
+    augeas { 'grub.conf/update password':
+      incl    => '/boot/grub/grub.conf',
+      lens    => 'grub.lns',
+      changes => [
+        "set password '${grubpassword}'",
+      ],
+      require => Augeas['grub.conf/insert password'],
+    }
+
+  }
 }
