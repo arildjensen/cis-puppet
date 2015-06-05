cfc59529548028c3cf4309b0bfe6caa31547c1dc
Proletaryo Cabales <proletaryo@gmail.com>
2015-06-05 11:59:04 +0000

    added/modified CIS guidelines 4.x.x for AWS Linux

---
+++ manifests/awslinux/4_6.pp
@@ -0,0 +1,8 @@
+# Class cis::awslinux::4_6
+#
+# CIS Security Benchmark for RHEL7
+#
+
+class cis::awslinux::4_6     {
+  include cis::linuxcontrols::c0041
+}
--- manifests/linuxcontrols/c0041.pp
+++ manifests/linuxcontrols/c0041.pp
@@ -4,29 +4,44 @@
 #
 
 class cis::linuxcontrols::c0041 {
-  case $operatingsystemmajrelease {
-    6: {
-      package {'iptables':
+  case $::operatingsystem {
+    'RedHat': {
+      case $operatingsystemmajrelease {
+        6: {
+          package {'iptables':
+            ensure => installed,
+            before => Service['iptables'],
+          }
+          service {'iptables':
+            ensure     => running,
+            enable     => true,
+            hasrestart => true,
+            hasstatus  => true,
+          }
+        }
+        7: {
+          package {'firewalld':
+            ensure => installed,
+            before => Service['firewalld'],
+          }
+          service {'firewalld':
+            ensure     => running,
+            enable     => true,
+            hasrestart => true,
+            hasstatus  => true,
+          }
+        }
+      }
+    }
+    'Amazon': {
+      package { 'iptables':
         ensure => installed,
         before => Service['iptables'],
       }
-      service {'iptables':
+
+      service { 'iptables':
         ensure     => running,
         enable     => true,
-        hasrestart => true,
-        hasstatus  => true,
-      }
-    }
-    7: {
-      package {'firewalld':
-        ensure => installed,
-        before => Service['firewalld'],
-      }
-      service {'firewalld':
-        ensure     => running,
-        enable     => true,
-        hasrestart => true,
-        hasstatus  => true,
       }
     }
   }
